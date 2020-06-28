5f8758054879d61e9aeef71b89b76d85fe425c12
S.Lukeman Hakkim <lukemanhakkim@gmail.com>
2020-06-28 13:20:28 -0400

    Task3 Bandwidth monitering Solutions
    Task3 Bandwidth monitering Solutions

---
+++ Task3/Task3_BandwidthMonitering.ps1
@@ -0,0 +1,28 @@
+﻿<# 
+The script does the following task:
+1. Upload/download speed
+2. Latency of any server
+3. Bandwidth usage
+4. List of VPN information
+5. Destination VPN hostname, IP and Mac address
+#> 
+
+#Upload/Download speed
+Start-SpeedTest
+
+#To check the latency of given server
+$latencyCheck = Test-Connection autowit.co
+$latencyCheck
+
+#Bandwidth usage
+get-netadapterstatistics
+
+#VPN info to get hostname and IP
+Get-VpnConnection
+
+#Using the hostname and IP from the above command to get the Mac adddress of VPN
+(gwmi -ComputerName DNSNAMEORIPADDRESS -Class Win32_NetworkAdapterConfiguration | where {$_.IPAddress -like '<IP>'}).MACAddress
+
+
+
+
